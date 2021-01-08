3e010afa11d2aeae51cc6c83c6b19ae5a51d4657
46398295 <strelcov.stanislav93@gmail.com>
2021-01-08 11:30:17 +0200

    fix: remove code that is never used

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,5 @@
 "use strict";
-/*************************************pixPer *****************************************/
 
-var body = document.getElementById('page');
-var pixPer = document.querySelector('.pixPer');
-
-function increaseOpacity() {
-    let style, opacityAtStart, opacityAtEnd;
-
-    style = getComputedStyle(body);
-    opacityAtStart = parseFloat(style.opacity);
-    opacityAtStart += 0.2;
-    opacityAtEnd = opacityAtStart.toString();
-    body.style.opacity = opacityAtEnd;
-}
-function decreaseOpacity() {
-    let style, opacityAtStart, opacityAtEnd;
-    
-    style = getComputedStyle(body);
-    opacityAtStart = parseFloat(style.opacity);
-    opacityAtStart -= 0.2;
-    opacityAtEnd = opacityAtStart.toString();
-    body.style.opacity = opacityAtEnd;
-}
-function toogleMockUp () {
-    let pStyle = getComputedStyle(pixPer);
-    if (pStyle.backgroundImage == 'none') {
-        console.log(pStyle.backgroundImage);
-        pixPer.style.backgroundImage = 'url("' + 'images/04_SPA_main.jpg' + '")';
-        body.style.opacity = '0.7';
-        console.log(pixPer.style.backgroundImage);
-    }
-    else {
-        pixPer.style.backgroundImage = 'none';
-        body.style.opacity = '1';
-    }
-}
-
-/**********************************************************************************/
 var brgBtn           = document.querySelector( '.burger-btn' );
 var headerMenu       = document.querySelector( '.header__menu' );
 var feautureItem     = document.querySelectorAll( '.feature__item' );
