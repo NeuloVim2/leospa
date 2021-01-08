118fbeedf840d83f7d6f28fb23fa8835c1b7e6a9
46398295 <strelcov.stanislav93@gmail.com>
2021-01-08 16:34:42 +0200

    refactor(feature__item): add to style.css hover effect and remove hover evaluation from script

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,9 @@
 
 var brgBtn           = document.querySelector( '.burger-btn' );
 var headerMenu       = document.querySelector( '.header__menu' );
-var feautureItem     = document.querySelectorAll( '.feature__item' );
-var feautureContent  = document.querySelectorAll( '.feature__content' );
 var testimonialsCont = document.querySelector( '.testimonials__content' );
 var testimonialsItems = testimonialsCont.getElementsByClassName("testimonials__item");
 
-for (let i = 0; i < feautureItem.length; i++) {
-    feautureItem[i].addEventListener ( 'mouseover', function () {
-        feautureContent[i].style.display = "block";
-    })
-    feautureItem[i].addEventListener ( 'mouseout', function () {
-        feautureContent[i].style.display = "none";      
-
-    })
-}
 
 /************/
 brgBtn.addEventListener( 'click', function() {
