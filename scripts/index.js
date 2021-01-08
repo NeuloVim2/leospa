47745fc0497d882df70efc12f3a31b30c16d80f8
46398295 <strelcov.stanislav93@gmail.com>
2021-01-08 19:42:32 +0200

    feat(testimonials slider): add jQuery Slick Plugin as testimonials's slider

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,31 @@
 "use strict";
 
-const brgBtn           = $( '.burger-btn' );
-const headerMenu       = $( '.header__menu' );
+const brgBtn               = $( '.burger-btn' ); // Get .burger-btn element from DOM
+const headerMenu           = $( '.header__menu' ); // Get .header__menu element from DOM
+const sliderTestimonials   = $('.testimonials__slider'); // Get .testimonials__slider element from DOM
 
-/************/
 $(document).ready(function() {
+
+    //Set click event listener on burger button
     brgBtn.click(function() {
+      
+      //Toggle burger button style on click
       brgBtn.toggleClass("active");
+
+      //Toggle headerMenu visability on click
       headerMenu.toggleClass("active");
   });
+
+  // Slider Slick initialisation
+  sliderTestimonials.slick({
+
+      slidesToShow: 1, // Show one slide per time
+      touchMove: true, // Enabele slides moving with a dinger on mobile device
+      dots: true,      // Show dots 
+      autoplay: true,  // Enable autoplay
+      autoplaySpeed: 3000,  // Set autoplay speed
+      dotsClass: "testimonials__controller" // Set custom class on dots
+
+  })
 });
+
