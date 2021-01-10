5623c15d9ab3a4c28d75740bca368193e80f80b0
46398295 <strelcov.stanislav93@gmail.com>
2021-01-10 17:39:24 +0200

    fix(timepicker): solve issue of not displaying timepicker at IE

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
 "use strict";
 
-const brgBtn               = $( '.burger-btn' ); // Get .burger-btn element from DOM
-const headerMenu           = $( '.header__menu' ); // Get .header__menu element from DOM
-const sliderTestimonials   = $('.testimonials__slider'); // Get .testimonials__slider element from DOM
+const brgBtn               = $( '.burger-btn' ); // Get .burger-btn element from header section for mobile
+const headerMenu           = $( '.header__menu' ); // Get .header__menu element from header section
+const sliderTestimonials   = $('.testimonials__slider'); // Get .testimonials__slider element from testimonials sections
+const datepicker           = $('#datepicker'); // Get #datepicker element from appointment section
+const timepicker           = $('#timepicker'); // Get #timepicker element from appointment section
 
 $(document).ready(function() {
 
@@ -16,7 +18,7 @@ $(document).ready(function() {
       headerMenu.toggleClass("active");
   });
 
-  // Slider Slick initialisation
+  // Slider 'Slick' initialisation
   sliderTestimonials.slick({
 
       slidesToShow: 1, // Show one slide per time
@@ -26,6 +28,18 @@ $(document).ready(function() {
       autoplaySpeed: 3000,  // Set autoplay speed
       dotsClass: "testimonials__controller" // Set custom class on dots
 
-  })
+  });
+
+  // Datepicker 'Pickaday' initialisation
+  datepicker.pikaday({
+    firstDay: 1,
+    minDate: new Date(2000, 0, 1),
+    maxDate: new Date(2020, 12, 31),
+    yearRange: [2000,2020]
+  });
+
+  // Timepicker 'Timepicker' initialisation
+  timepicker.wickedpicker();
+
 });
 
